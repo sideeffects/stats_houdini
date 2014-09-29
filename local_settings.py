3c008e12af3e8f92582a5a7011162f3f4f28091a
Yele Bonilla <yele@sidefx.com>
2014-09-29 17:20:13 -0400

    Adding menu options and reports names to the local settings.

--- local_settings.py
+++ local_settings.py
@@ -5,3 +5,44 @@ STATS_APPLICATIONS += (
 REPORT_MODULES += (
      'houdini_stats.reports.houdini',
 )
+
+menu_and_report_options = OrderedDict([
+    ("houdini", {
+        "menu_url_prefix": "houdini",
+        "menu_name": "Houdini",
+        "menu_options": [
+            ("usage", "Usage", [
+                "NewMachinesOverTime",
+                "MachinesActivelySendingStats",
+                "AvgNumConnectionsFromSameMachine",
+            ]),
+            ("uptime", "Session Information", [
+                "AverageSessionLength",
+                "AverageUsageByMachine",
+                "BreakdownOfApprenticeUsage",
+            ]),
+            ("crashes", "Crashes",[
+                "NumCrashesOverTime",
+                "NumOfMachinesSendingCrashesOverTime",
+                "AvgNumCrashesFromSameMachine",
+                "CrashesByOS",
+                "CrashesByProduct",
+            ]),
+            ("tools_usage", "Shelf & Tab menu Tools", [
+                "MostPopularTools",
+                "MostPopularToolsShelf",
+                "MostPopularToolsViewer",
+                "MostPopularToolsNetwork",
+            ]),
+            ("versions_and_builds", "Versions and builds",[
+                "VersionsAndBuilds",
+                "VersionsAndBuildsApprentice",
+                "VersionsAndBuildsCommercial",
+            ]),
+        ],
+        "groups": ['staff', 'r&d'],
+    }),                        
+])
+   
+TOP_MENU_OPTIONS = OrderedDict(TOP_MENU_OPTIONS.items() + \
+                               menu_and_report_options.items())                            
